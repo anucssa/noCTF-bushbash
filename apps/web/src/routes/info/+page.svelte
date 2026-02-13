f421a1fcbafd17b364ec936a7384aba313c22c60
Adrian Carpio <u7418332@anu.edu.au>
2026-02-13 15:58:53 +1100

    jeoirwioejr

---
+++ apps/web/src/routes/info/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+  import configState from "$lib/state/config.svelte";
+</script>
+
+<div class="h-80 flex justify-center items-end home_div" id="info_div_container">
+  <div id="info_div">
+  <h2 class="text-4xl"><strong>
+    The ANU's First O-Week Capture the Flag
+Competition!
+</strong>
+  </h2>
+  <br>
+  <ul class="text-xl">
+    <li>Beginner-friendly Computer Security challenges</li>
+    <li>Teams of up to <strong>6</strong> players</li>
+    <li><strong>$300</strong> total prize value for top 3 teams</li>
+  </ul>
+<br>
+  <div id="start_end_holder" class="flex press-start-2p">
+    <div id="start_holder">
+      <span id="start_title" class="time_title">Start</span><br>
+      <span id="start_duration" class="time_duration">18 Feb 2pm</span>
+    </div>
+    <div id="end_holder">
+      <span id="end_title" class="time_title">End</span><br>
+      <span id="end_duration" class="time_duration">22 Feb 2pm</span>
+    </div>
+  </div>
+<br>
+  <hr>
+<br>
+  <h2 class="text-4xl"><strong>
+    Rules
+  </strong></h2><br>
+  <ol>
+    <li>TODO</li>
+    <li>TODO</li>
+    <li>TODO</li>
+    <li>TODO</li>
+    <li>TODO</li>
+  </ol>
+  <br>
+  <h2 class="text-4xl"><strong>
+    Prizes
+  </strong></h2><br>
+  TODO
+  <br>
+  <br>
+    <hr>
+  <br>
+  <h2 class="text-4xl"><strong>
+    About the CSSA
+  </strong></h2><br>
+  TODO
+  </div>
+</div>
