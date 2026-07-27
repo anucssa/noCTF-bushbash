e931a40ce3b6bf112f69d9e3350350dcbd227e10
Adrian Carpio <u7418332@anu.edu.au>
2026-07-27 22:36:33 +1000

    added thingo

---
+++ apps/web/src/routes/ouTs1De/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import configState from "$lib/state/config.svelte";
+  import { difficultyToBgColour } from "$lib/utils/challenges";
+  import { type Difficulty } from "$lib/constants/difficulties";
+</script>
+
+<div class="h-80 flex justify-center items-end home_div" id="info_div_container">
+  <script src="https://kit.fontawesome.com/8ac6ea753d.js" crossorigin="anonymous"></script>
+  <div id="outside-div">
+    working on it...
+  </div>
+</div>
