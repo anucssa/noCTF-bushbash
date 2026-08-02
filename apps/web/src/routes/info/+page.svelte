3fdcd54e9a95e1d6e03440bd256515419918a3a7
Adrian Carpio <u7418332@anu.edu.au>
2026-08-02 16:05:33 +1000

    wrote credits

--- apps/web/src/routes/info/+page.svelte
+++ apps/web/src/routes/info/+page.svelte
@@ -82,70 +82,70 @@
   <br>
     <hr>
   <br>
-  <!--
   <h2 class="text-4xl"><strong>
     Credits
   </strong></h2><br>
-  <strong>Peter Woodhead</strong> - <i>"PetePete_Repeat"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("medium")}`}>Infra</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Communications</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Sponsorships</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Organiser</div>
+  <strong>Peter Woodhead</strong> - <i>"PetePete_Repeat a.k.a gr33nMari0"</i>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("medium")}`}>Infra</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Communications</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Sponsorships</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Organiser</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://github.com/gr33nMari0" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://www.linkedin.com/in/peterwoodhead" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-linkedin logos"></i>&nbsp;&nbsp;<span class="logos link">Linkedin</span></a>
   <br><br>
   <strong>Alyssa Herald</strong> - <i>"CraftyDH"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("medium")}`}>Infra</div>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("medium")}`}>Infra</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://github.com/CraftyDH" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
   <br><br>
   <strong>Adrian Carpio</strong> - <i>"oreophone"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("medium")}`}>Website</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Art & Design</div>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("medium")}`}>Website</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Art & Design</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://github.com/oreophone" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://www.linkedin.com/in/adrian-carpio-8a3a523b0/" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-linkedin logos"></i>&nbsp;&nbsp;<span class="logos link">Linkedin</span></a>
   <br><br>
   <strong>Harold Gao</strong> - <i>"Friska"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Communications</div>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("easy")}`}>Communications</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://github.com/LaFriska" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
   <br><br>
-  <strong>Elliot</strong> - <i>"shminge"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div>
+  <strong>Elliott</strong> - <i>"shminge"</i>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://github.com/shminge" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
+  <br><br>
+  <strong>Ben Pye</strong> - <i>"loki314"</i>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("beginner")}`}>CSSA</div><div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div>
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href="https://github.com/Loki314159" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
   <br><br><br>
-  Many thanks to our volunteers & problem writers! :D
+  Many thanks to our volunteers & Challenge Writers! :D
   <br>
   <br>
 
-  <strong>Ben Pye</strong> - <i>"loki314"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div>
-  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <a href="https://github.com/Loki314159" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
-  <br><br>
   <strong>Cameron</strong> - <i>"QuadroTec"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://github.com/QuadroTec" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
   <br><br>
-  <strong>Charlotte</strong> - <i>"charli.dev"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div>
+  <strong>Alec Smith</strong> - <i>"Vals"</i>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <a href="https://github.com/sfulham" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href="https://www.linkedin.com/in/alec-smith-548930246" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-linkedin logos"></i>&nbsp;&nbsp;<span class="logos link">Linkedin</span></a>
   <br><br>
-  <strong>Eric Cao</strong> - <i>"Willsike"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div>
+  <strong>Thomas Fahey</strong> - <i>"Thomas"</i>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <a href="https://github.com/willsike1" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href="https://www.linkedin.com/in/thomashfahey?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-linkedin logos"></i>&nbsp;&nbsp;<span class="logos link">Linkedin</span></a>
   <br><br>
   <strong>Jaidyn Johnston</strong> - <i>"justjayiguess"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://github.com/JustJayIGuess" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
   <br><br>
   <strong>Long Feng</strong> - <i>"Eisverygoodletter"</i>
-  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Problem Writer</div>
+  <div class={`badge py-2 font-semibold pop ${difficultyToBgColour("hard")}`}>Challenge Writer</div>
   <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <a href="https://github.com/Eisverygoodletter" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-square-github logos"></i>&nbsp;&nbsp;<span class="logos link">Github</span></a>&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href="https://www.linkedin.com/in/yuetlong-feng-8a01842ab" target="_blank"  rel="noopener noreferrer"><i class="fa-brands fa-linkedin logos"></i>&nbsp;&nbsp;<span class="logos link">Linkedin</span></a>
   <br>
 
   <br><br>
@@ -166,6 +166,5 @@
   <br>
   Note that any clarifications/challenge updates will be announced publicly on this website.
   Announcements may be viewed on the <a class="link" title="Challenge Screen" href="/challenges">Challenges</a> screen.
-    -->
   </div>
 </div>
